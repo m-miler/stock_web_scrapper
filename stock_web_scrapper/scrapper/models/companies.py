95b6dfb354a6bed7befc93add4b69ad30330bf4c
m-miler <mmiler30@gmail.com>
2023-08-26 18:11:08 +0200

    fix: change length check in company_name and index columns

--- stock_web_scrapper/scrapper/models/companies.py
+++ stock_web_scrapper/scrapper/models/companies.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class StockCompanies(models.Model):
     company_full_name = models.CharField(
-        max_length=50, help_text="Full Name of the Company"
+        max_length=200, help_text="Full Name of the Company"
     )
     company_abbreviation = models.CharField(
         max_length=10, help_text="Company Abbreviation", unique=True
     )
     index = models.CharField(
-        max_length=20, help_text="Company Stock Exchange Market Index"
+        max_length=200, help_text="Company Stock Exchange Market Index"
     )
 
     def __str__(self):
