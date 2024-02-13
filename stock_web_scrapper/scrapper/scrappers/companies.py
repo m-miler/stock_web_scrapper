8976dbd4b99c43c3da93dabd1480ebce698c9b9c
m-miler <mmiler30@gmail.com>
2024-02-13 23:13:42 +0100

    refactor: code refactoring

--- stock_web_scrapper/scrapper/scrappers/companies.py
+++ stock_web_scrapper/scrapper/scrappers/companies.py
@@ -31,62 +31,64 @@ class CompaniesScrapper:
 
         return driver
     def update(self):
-        self._get_companies()
-        for company in self.companies_list:
-            self.save(company)
+        companies_list: list[dict] = self._get_companies()
+        for company in companies_list:
+            self._save(company)
 
     @staticmethod
-    def save(company_data: tuple) -> None:
+    def _save(company_info: dict[str]) -> None:
         """
         Function to save web scrapped data to the companies table in database.
-        :param company_data: companies info data
+        :param company_info: companies info data
         :return: None
         """
         obj, created = StockCompanies.objects.update_or_create(
-            company_full_name=company_data[0],
-            company_abbreviation=company_data[1],
-            index=company_data[2]
+            company_full_name=company_info['company_name'],
+            company_abbreviation=company_info['ticker'],
+            index=company_info['index']
         )
 
-    def _get_companies(self) -> None:
+    def _get_companies(self) -> list[dict]:
         """
         Function with web scrapping code to get the companies information data.
         :return: list[tuple[str, str, str]]
         """
+        companies_list: list[dict] = []
         data = self._get_companies_table()
         companies = data.findAll(lambda tag: tag.name == 'tr')
 
         for company in companies[1:]:
-            company_name: str = company.find(attrs={'class': 'name'}).find(text=True).strip()
-            ticker: str = company.find(attrs={'class': 'name'}).find('span').text.strip()[1:4]
-            index: str = self._split_index(company.text)
-            company_info: tuple[str, str, str] = (company_name, ticker, index)
-            self.companies_list.append(company_info)
+            company_info: dict[str, str, str] = {
+                'company_name': company.find(attrs={'class': 'name'}).find(text=True).strip(),
+                'ticker': company.find(attrs={'class': 'name'}).find('span').text.strip()[1:4],
+                'index': self._split_index(company.text)}
+            companies_list.append(company_info)
+
+        return companies_list
 
     def _get_companies_table(self) -> bs4.Tag:
         """
-        Method to get full companies list from GPW website.
+        Method to get full companies list from the website.
         :return: bs4.Tag
         """
-
-        click_limit = self._calculate_click_limit()
+        driver: webdriver = self._prepare_webdriver()
+        click_limit = self._calculate_click_limit(driver)
 
         for i in range(1, click_limit):
-            show_more = self.driver.find_element(By.XPATH, self.show_more_txt)
-            self.driver.execute_script("arguments[0].click();", show_more)
+            show_more = driver.find_element(By.XPATH, self.show_more_txt)
+            driver.execute_script("arguments[0].click();", show_more)
             time.sleep(3)
 
-        request = self.driver.page_source
+        request = driver.page_source
         companies_table = BeautifulSoup(request, "html.parser").\
             find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == 'lista-spolek')
 
         return companies_table
 
-    @staticmethod
-    def _split_index(text: str) -> str:
+    def _split_index(self, text: str) -> str:
         return ";".join([x.strip() for x in text.split('\n\n\n')[2].split("|")[1].split(",")])
 
-    def _calculate_click_limit(self):
-        companies_number = int(self.driver.find_element(By.ID, 'count-all').text)
-        data_limit = int(self.driver.find_element(By.CLASS_NAME, 'more').get_attribute('data-limit'))
+    def _calculate_click_limit(self, driver: webdriver):
+        companies_number = int(driver.find_element(By.ID, 'count-all').text)
+        data_limit = int(driver.find_element(By.CLASS_NAME, 'more').get_attribute('data-limit'))
         return int(round((companies_number - data_limit) / data_limit, 0))
