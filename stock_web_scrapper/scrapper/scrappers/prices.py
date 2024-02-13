7cd78751308724de8a6a66c644868e24ef3621bf
m-miler <mmiler30@gmail.com>
2024-02-13 23:15:00 +0100

    refactor: code refactoring

--- stock_web_scrapper/scrapper/scrappers/prices.py
+++ stock_web_scrapper/scrapper/scrappers/prices.py
@@ -26,8 +26,6 @@ class PriceScrapper:
         self.end = end
         self.interval = interval
         self.pause = pause
-        self.url = \
-            f"https://stooq.pl/q/{self.interval}/l/?s={self.ticker}&d1={self.start}&d2={self.end}&i={self.interval}"
         self.request_counter = 0
         self.request_limit = request_limit
 
