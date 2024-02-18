09ec95f8644e166978d50b01b7dc01b87b54199a
m-miler <mmiler30@gmail.com>
2024-02-18 15:14:45 +0100

    chore: add sample dev env to postgres db

--- stock_web_scrapper/core/env.py
+++ stock_web_scrapper/core/env.py
@@ -6,10 +6,10 @@ env = environ.Env(
     SECRET_KEY=(str, get_random_secret_key()),
     # POSTGRES
     POSTGRES=(bool, True),
-    DB_NAME=(str, "postgres"),
+    DB_NAME=(str, "stocks"),
     DB_USER=(str, "postgres"),
     DB_PASSWORD=(str, "postgres"),
-    DB_HOST=(str, "localhost"),
+    DB_HOST=(str, "db"),
     DB_PORT=(str, "5432"),
     # Celery
     CELERY_BROKER=(str, "redis://redis:6379/0"),
