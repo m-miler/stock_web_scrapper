6391591dd3ff8978f577149ee93e2c3f94538a6d
m-miler <mmiler30@gmail.com>
2024-02-19 07:54:25 +0100

    chore: dev docker optimization

--- entrypoint_dev.sh
+++ entrypoint_dev.sh
@@ -14,5 +14,6 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 python manage.py update_companies
+python manage.py runserver 0.0.0.0:8080
 
 exec "$@"
--- stock_web_scrapper/scrapper/management/commands/update_companies.py
+++ stock_web_scrapper/scrapper/management/commands/update_companies.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-from scrapper.scrappers.companies import CompaniesScrapper
+from scrapper.tasks import update_companies
 
 
 class Command(BaseCommand):
     help = 'Populate database with stock companies name from GPW site.'
 
     def handle(self, *args, **options):
-        CompaniesScrapper().update()
+        update_companies()
