4bd57fc03e25315644a1c9000472d0505189ff6c
m-miler <mmiler30@gmail.com>
2024-02-21 11:51:36 +0100

    refactor: change the dev entrypoint script, for now update_companies needs to be run manually from container

--- entrypoint_dev.sh
+++ entrypoint_dev.sh
@@ -11,9 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
 python manage.py migrate
-python manage.py update_companies
 python manage.py runserver 0.0.0.0:8080
 
 exec "$@"
