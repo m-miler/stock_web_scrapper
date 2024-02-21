8d7981782e82059b368e4faef51b0feead7cc5d8
m-miler <mmiler30@gmail.com>
2024-02-21 11:57:17 +0100

    fix: change wrong type annotation in tasks and allow to get list from the request arguments

--- stock_web_scrapper/API/views/scrapper.py
+++ stock_web_scrapper/API/views/scrapper.py
@@ -6,7 +6,7 @@ from scrapper.tasks import update_stock_prices
 class Scrapper(ModelViewSet):
 
     def stock_update(self, request) -> Response:
-        ticker = request.GET.get('s')
+        ticker = request.GET.getlist('s')
         start_date = request.GET.get('d1', None)
         end_date = request.GET.get('d2', None)
         interval = request.GET.get('i', None)
