b5a3df079595f564f81fc1abce3cc78ab9011dfa
m-miler <mmiler30@gmail.com>
2024-03-03 22:43:27 +0100

    feat: add endpoint to find last known price for company

--- stock_web_scrapper/API/views/prices.py
+++ stock_web_scrapper/API/views/prices.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import GenericViewSet
 from scrapper.models.prices import StockPrices
 from ..serializers.prcies import PricesSerializer
 from ..filters.prices import PricesFilters
+from rest_framework.response import Response
+from django.http.response import JsonResponse
 
 
 class PricesList (ListModelMixin, GenericViewSet):
@@ -12,3 +14,8 @@ class PricesList (ListModelMixin, GenericViewSet):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+    def get_last_price(self, request):
+        last_price = StockPrices.objects.filter(
+            company_abbreviation=request.query_params.get('ticker')).latest('date')
+        return JsonResponse({'last_price': last_price.close_price})
